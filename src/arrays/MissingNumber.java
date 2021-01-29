b1d5946bf4f8263bfb2dbe6b064be2229c09c94f
imadcollin <imad.collin@gmail.com>
2021-01-29 15:47:33 +0100

    Add second method for missing number

--- src/arrays/MissingNumber.java
+++ src/arrays/MissingNumber.java
@@ -5,10 +5,11 @@ public class MissingNumber {
         int arr[] = { 1, 2, 3, 4, 6 }; // 5 is missing.
 
         System.out.println(missingNumber(arr));
-        
+        System.out.println(missingNumberForLoop(arr));
+
         int arr2[] = { 1, 3 }; // 2 is missing
         System.out.println(missingNumber(arr2));
-
+        System.out.println(missingNumberForLoop(arr2));
     }
 
     public static int missingNumber(int arr[]) {
@@ -22,5 +23,14 @@ public class MissingNumber {
             sum += i;
         }
         return ArraySum - sum;
+    }
+
+    public static int missingNumberForLoop(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int prev = arr[i] + 1;
+            if (prev != arr[i + 1])
+                return prev;
+        }
+        return 0;
     }
 }
