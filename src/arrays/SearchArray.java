5a179187a2f74f0266cb5b9b3dd82b6bff1c9995
imadcollin <imad.collin@gmail.com>
2021-02-04 16:55:53 +0100

    Add binary search method

--- src/arrays/SearchArray.java
+++ src/arrays/SearchArray.java
@@ -6,7 +6,34 @@ public class SearchArray {
 
         // Search for 5 index
         int index = search(arr, 5);
+        // System.out.println(index);
+
+        int arr2[] = { 1, 2, 3, 4, 5, 6, 7, 12, 15, 22 };
+        index = binarySearch(arr2, 6);
         System.out.println(index);
+
+    }
+
+    public static int binarySearch(int arr[], int num) {
+        int low = 0, high = 0;
+        low = arr[0];
+        high = arr.length - 1;
+        do {
+            int mid = (high + low) / 2;
+
+            if (arr[mid] == num) {
+                return mid;
+            }
+
+            else if (arr[mid] < num) {
+                low = mid + 1;
+
+            } else if (arr[mid] > num) {
+                high = mid - 1;
+            }
+
+        } while (high >= low);
+        return -1;
     }
 
     public static int search(int arr[], int element) {
