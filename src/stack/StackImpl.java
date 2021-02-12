e5230115ff5e2a653ace0ad5489d868092f5427a
imadcollin <imad.collin@gmail.com>
2021-02-12 21:51:55 +0100

    Refactor stack implemntation

--- src/stack/StackImpl.java
+++ src/stack/StackImpl.java
@@ -3,41 +3,42 @@ package stack;
 import java.util.Arrays;
 
 public class StackImpl {
-    public int size = 0;
-    public int capacity = 0;
+    public int size;
+    public int top;
     public int arr[];
 
-    public StackImpl(int capacity) {
-        this.capacity = capacity;
-        this.arr = new int[capacity];
+    public StackImpl(int size) {
+        this.top = -1;
+        this.size = size;
+        this.arr = new int[size];
     }
 
     public void push(int el) {
-        if (hasScpace())
-            arr[size++] = el;
-        else {
+        if (hasSpace()) {
+            arr[++top] = el;
+        } else {
             increaseCapacity();
-            arr[size++] = el;
+            arr[++top] = el;
         }
     }
 
     public int peek() {
         if (size <= 0)
             throw new IllegalArgumentException();
-        return arr[size - 1];
+        return arr[top];
     }
 
     public int size() {
         return size;
     }
 
-    public boolean hasScpace() {
-        return capacity > size;
+    public boolean hasSpace() {
+        return top < size - 1;
     }
 
     public void increaseCapacity() {
-        capacity *= 2;
-        arr = Arrays.copyOf(arr, capacity);
+        size *= 2;
+        arr = Arrays.copyOf(arr, size);
     }
 
 }
