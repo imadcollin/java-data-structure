a1f814cb331117a268d21f22cf8b6d620620f271
imadcollin <imad.collin@gmail.com>
2021-02-12 21:52:06 +0100

    Test stack functions

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -1,25 +1,24 @@
 package stack;
 
 public class Main {
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    StackImpl stack =new StackImpl(5); 
+        StackImpl stack = new StackImpl(5);
 
-    stack.push(1);
-    stack.push(2);
+        stack.push(1);
+        stack.push(2);
 
-    System.out.println(stack.size);
-    System.out.println(stack.capacity);
-    System.out.println(stack.peek());
+        System.out.println(stack.size);
+        System.out.println(stack.peek());
 
-    System.out.println("-----------------");
-    stack.push(5);
-    stack.push(4);
-    stack.push(3);
-    stack.push(2);
+        System.out.println("-----------------");
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(3);
 
-    System.out.println(stack.size);
-    System.out.println(stack.capacity);
-    System.out.println(stack.peek());
-}    
+        System.out.println(stack.size);
+        System.out.println(stack.peek());
+    }
 }
