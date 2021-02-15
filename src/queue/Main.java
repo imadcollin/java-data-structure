b1622a068b34cba48ef770f66fe99749c1157f4a
imadcollin <imad.collin@gmail.com>
2021-02-15 23:33:40 +0100

    Print some logs for the queus

--- src/queue/Main.java
+++ src/queue/Main.java
@@ -6,7 +6,19 @@ import java.util.Queue;
 public class Main {
     public static void main(String[] args) {
 
-        queueUtil();
+        // queueUtil();
+        QueueImpl();
+    }
+
+    private static void QueueImpl() {
+        QueueImpl queue = new QueueImpl(3);
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        System.out.println(queue.size()); // 3
+        System.out.println(queue.poll()); // 1
+        System.out.println(queue.remove()); // 2
+        System.out.println(queue.peek()); // 3
 
     }
 
