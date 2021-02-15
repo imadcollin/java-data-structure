6ed3edbe44144ea27495b6a1bd2cc103a46d01e9
imadcollin <imad.collin@gmail.com>
2021-02-15 23:11:20 +0100

    Make queueutil function

--- src/queue/Main.java
+++ src/queue/Main.java
@@ -5,16 +5,21 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
+
+        queueUtil();
+
+    }
+
+    public static void queueUtil() {
         Queue queue = new LinkedList<>();
         queue.add(1);
         queue.add(2);
         queue.add(3);
 
-        System.out.println(queue.size());   // 3
-
-        System.out.println(queue.peek());   // 1
-        System.out.println(queue.poll());   // 1
+        System.out.println(queue.size()); // 3
+        System.out.println(queue.poll()); // 1
         System.out.println(queue.remove()); // 2
+        System.out.println(queue.peek()); // 3
     }
 
 }
