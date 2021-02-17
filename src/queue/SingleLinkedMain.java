4e59566c477b55d98ee3e31309d32e77b9b669eb
imadcollin <imad.collin@gmail.com>
2021-02-17 16:05:11 +0100

    Print some output for removing

--- src/queue/SingleLinkedMain.java
+++ src/queue/SingleLinkedMain.java
@@ -8,9 +8,29 @@ public class SingleLinkedMain {
         singleLinked.addNode(3);
 
         singleLinked.print();
-        singleLinked.removeNode(2);
         System.out.println("---------------");
-        singleLinked.print();
         System.out.println("Node ID: "+ singleLinked.getNodeId(5));
+        System.out.println("---------------");
+        singleLinked.removeFirstNode();
+        System.out.println("-------Removed head--------");
+        singleLinked.print();
+        
+        System.out.println("-------Empty--------");
+        singleLinked.removeFirstNode();
+        singleLinked.removeFirstNode();
+        singleLinked.print();
+        
+        
+        QueueLinkedList singleLinked2= new QueueLinkedList(); 
+        singleLinked2.addNode(1);
+        singleLinked2.addNode(2);
+        singleLinked2.addNode(3);
+        System.out.println("-------List--------");
+        singleLinked2.print();
+        
+        System.out.println("-------Remove Last--------");
+        singleLinked2.removeLastNode();
+        singleLinked2.print();
+        
     }
 }
