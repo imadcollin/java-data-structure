af43a6854a9b51caa659305fa575f0e6b1b855fb
imadcollin <imad.collin@gmail.com>
2021-02-19 23:33:17 +0100

    Print some output

---
+++ src/queue/doubleLink/DoubleLinkMain.java
@@ -0,0 +1,10 @@
+package queue.doubleLink;
+
+public class DoubleLinkMain {
+    public static void main(String[] args) {
+        DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+        doubleLinkedList.inseretFirst(1);
+        doubleLinkedList.inseretFirst(2);
+        doubleLinkedList.print();
+    }
+}
