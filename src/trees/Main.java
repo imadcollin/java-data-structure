2cb40c745e37091f03476fa110386b7c42277e55
imadcollin <imad.collin@gmail.com>
2021-02-24 14:54:25 +0100

    Add main file

---
+++ src/trees/Main.java
@@ -0,0 +1,13 @@
+package trees;
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        bt.buildTree(1);
+        bt.buildTree(2);
+        bt.buildTree(3);
+        bt.buildTree(7);
+        bt.buildTree(6);
+
+    }
+}
