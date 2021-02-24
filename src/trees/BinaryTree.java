ebda86361f7a6e38a2eba09182fc8864e4b1f92a
imadcollin <imad.collin@gmail.com>
2021-02-24 14:53:49 +0100

    Initial tree implementation

---
+++ src/trees/BinaryTree.java
@@ -0,0 +1,28 @@
+package trees;
+
+public class BinaryTree {
+    Node root;
+    Boolean visit = false;
+
+    public Node addToRoot(Node current, int data) {
+
+        if (current == null) {
+            return new Node(data);
+        }
+        if (current.data > data) {
+            current.right = addToRoot(current.right, data);
+        }
+
+        else if (current.data < data) {
+            current.left = addToRoot(current.left, data);
+        } else {
+            return current;
+        }
+        return current;
+    }
+
+    public void buildTree(int data) {
+        root = addToRoot(root, data);
+    }
+
+}
