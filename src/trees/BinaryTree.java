146476dbf3923eb690e95f9ee87f183f25d4ace1
imadcollin <imad.collin@gmail.com>
2021-02-25 15:03:35 +0100

    Add contians method

--- src/trees/BinaryTree.java
+++ src/trees/BinaryTree.java
@@ -25,12 +25,27 @@ public class BinaryTree {
         root = addToRoot(root, data);
     }
 
-    public void trasverse(Node node){
-        if(node !=null){
+    public void trasverse(Node node) {
+        if (node != null) {
             trasverse(node.left);
-            System.out.println("The data is :"+node.data);
+            System.out.println("The data is :" + node.data);
             trasverse(node.right);
-        } 
+        }
+    }
+
+    public boolean contian(Node node, int data) {
+
+        if (node == null) {
+            return false;
+        }
+        if (node.data == data) {
+            return true;
+        }
+        if (node.data > data)
+            return contian(node.left, data);
+        else
+            return (contian(node.right, data));
+
     }
 
 }
