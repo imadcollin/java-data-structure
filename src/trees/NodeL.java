b5a7331b8b8e74044f5e58ad2d1bb40d1c3312d7
imadcollin <imad.collin@gmail.com>
2021-03-01 22:52:11 +0100

    Change to singleLinkedList impl

--- src/trees/NodeL.java
+++ src/trees/NodeL.java
@@ -1,47 +1,36 @@
 package trees;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class NodeL<T> {
+public class NodeL {
     int data;
     String name;
-    NodeL left;
-    NodeL right;
-    private NodeL<T> parent = null;
-    private List<NodeL<T>> childern = new ArrayList<>();
+    NodeL next;
 
     public NodeL(String name, int data) {
+        this.name = name;
+        this.data = data;
+        next = null;
+    }
+
+}
+
+class BinaryTree {
+    BinaryTree left, right;
+    int data;
+    String name;
+
+    public BinaryTree(String name, int data) {
         this.name = name;
         this.data = data;
         left = right = null;
     }
 
-    public NodeL addChild(String name, int data) {
-        NodeL newNodeL = new NodeL(name, data);
-        if (parent == null) {
-            parent = newNodeL;
-        } else {
-            if (data < parent.data) {
-                parent.left = newNodeL;
-                return newNodeL;
-            }
-            parent.right = newNodeL;
-            return newNodeL;
+    public NodeL push(NodeL head, int data) {
 
-        }
+        NodeL newNode = new NodeL(name, data);
 
-        this.childern.add(newNodeL);
-        System.out.println("test");
-        return newNodeL;
+        newNode.next = head;
+        head = newNode;
+        return head;
+
     }
 
-    public void print() {
-
-        this.childern.stream().forEach(node -> System.out.println("Name: " + node.name + " \t ID:" + node.data));
-    }
-
-    public int size() {
-        return childern.size();
-    }
 }
