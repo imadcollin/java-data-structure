2e85aa636f6797d28762d8c33a9877fd4df53c6d
imadcollin <imad.collin@gmail.com>
2021-03-02 18:29:06 +0100

    Print some data with stack impl

--- src/trees/PreOrderTra.java
+++ src/trees/PreOrderTra.java
@@ -19,17 +19,43 @@ public class PreOrderTra {
         if (node == null)
             return;
 
-        Stack<Node> stack = new Stack<>();
-
+        Stack<Node> stack = new Stack<Node>();
+        stack.push(node);
         while (!stack.isEmpty()) {
             Node temp = stack.pop();
             System.out.println(temp.data);
-            if (node.left != null) {
-                stack.push(temp);
+            if (temp.left != null) {
+                stack.push(temp.left);
             }
-            if (node.right != null) {
-                stack.push(temp);
+            if (temp.right != null) {
+                stack.push(temp.right);
             }
         }
+    }
+}
+
+class Print {
+    public static void main(String[] args) {
+
+        PreOrderTra preOrderTra = new PreOrderTra();
+
+        Node root = new Node(100);
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+
+        root.left = n1;
+        root.right = n2;
+        root.left.left = n3;
+        root.left.left.left = n4;
+        root.right.left = n5;
+        System.out.println(".......PreOrder Rec......");
+
+        preOrderTra.preOrder(root);
+        System.out.println(".......PreOrder Iter......");
+        preOrderTra.preOrderIterative(root);
+
     }
 }
