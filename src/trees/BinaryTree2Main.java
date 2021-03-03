3f67e38ea554b1b40376f97ebaa5112ab024b811
imadcollin <imad.collin@gmail.com>
2021-03-03 18:14:02 +0100

    Replace with meaningful name

---
+++ src/trees/BinaryTree2Main.java
@@ -0,0 +1,20 @@
+package trees;
+
+public class BinaryTree2Main {
+    public static void main(String[] args) {
+        BinaryTree2 binaryTree = new BinaryTree2();
+        binaryTree.push(1);
+        binaryTree.push(2);
+        binaryTree.push(3);
+        binaryTree.push(4);
+
+        System.out.println("--------Size----------");
+        System.out.println(binaryTree.size());
+        
+        System.out.println("--------Print----------");
+        binaryTree.print();
+        
+        System.out.println("-------Traversal--------");
+        binaryTree.travesel(binaryTree.root);
+    }
+}
