0e395df38cd68a3492b5d6d1ee705ea1f9515410
imadcollin <imad.collin@gmail.com>
2021-03-03 18:20:15 +0100

    Refactoring preOeder file

--- src/trees/PreOrderTra.java
+++ src/trees/PreOrderTra.java
@@ -39,23 +39,12 @@ class Print {
 
         PreOrderTra preOrderTra = new PreOrderTra();
 
-        Node root = new Node(100);
-        Node n1 = new Node(1);
-        Node n2 = new Node(2);
-        Node n3 = new Node(3);
-        Node n4 = new Node(4);
-        Node n5 = new Node(5);
+        System.out.println("-------PreOrder Rec--------");
 
-        root.left = n1;
-        root.right = n2;
-        root.left.left = n3;
-        root.left.left.left = n4;
-        root.right.left = n5;
-        System.out.println(".......PreOrder Rec......");
+        preOrderTra.preOrder(RootNode.getRootNodes());
 
-        preOrderTra.preOrder(root);
-        System.out.println(".......PreOrder Iter......");
-        preOrderTra.preOrderIterative(root);
+        System.out.println("-------PreOrder Iter--------");
+        preOrderTra.preOrderIterative(RootNode.getRootNodes());
 
     }
 }
