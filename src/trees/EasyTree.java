dbd1b1faccbbdb2a1d33a99fc65731133fd7bb05
imadcollin <imad.collin@gmail.com>
2021-03-03 18:27:13 +0100

    Refactoring easytree

--- src/trees/EasyTree.java
+++ src/trees/EasyTree.java
@@ -1,6 +1,5 @@
 package trees;
 
-
 public class EasyTree {
 
     Node root = null;
@@ -9,34 +8,17 @@ public class EasyTree {
         if (node != null) {
 
             traversal(node.left);
-            System.out.print(" -> "+ node.data);
+            System.out.print(" "+node.data);
             traversal(node.right);
         }
 
     }
 
 }
-// Already Defined. 
 
-// class Node {
-//     int data;
-//     Node left;
-//     Node right;
-
-//     public Node(int data) {
-//         this.data = data;
-//         left = right = null;
-//     }
-// }
-
-class Printer{
+class Printer {
     public static void main(String[] args) {
-        EasyTree easyTree = new EasyTree(); 
-        easyTree.root= new Node(1); 
-        easyTree.root.left= new Node(2); 
-        easyTree.root.left.left= new Node(3); 
-        easyTree.root.left.right= new Node(5);
-        
-        easyTree.traversal(easyTree.root);
+        EasyTree easyTree = new EasyTree();
+        easyTree.traversal(RootNode.getRootNodes());
     }
 }
