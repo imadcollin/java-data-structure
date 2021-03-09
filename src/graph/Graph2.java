dfe0573823ddcf21080672edc283fc1292b0b012
imadcollin <imad.collin@gmail.com>
2021-03-09 23:07:12 +0100

    Add main method for graph2

--- src/graph/Graph2.java
+++ src/graph/Graph2.java
@@ -1,6 +1,7 @@
 package graph;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,11 +9,12 @@ public class Graph2 {
     private Map<Vertex, List<Vertex>> adjsVerts;
 
     public Graph2() {
+        adjsVerts = new HashMap<>();
 
     }
 
     public void addVertex(String label) {
-        adjsVerts.putIfAbsent(new Vertex(label), new ArrayList<Vertex>());
+        adjsVerts.putIfAbsent(new Vertex(label), new ArrayList<>());
     }
 
     public void removeVertex(String label) {
@@ -25,5 +27,22 @@ public class Graph2 {
         Vertex v2 = new Vertex(label2);
         adjsVerts.get(v1).add(v2);
         adjsVerts.get(v2).add(v1);
+    }
+
+    public void countVertex() {
+        System.out.println(adjsVerts.keySet().size());
+    }
+
+}
+
+class MainGraph2 {
+    public static void main(String[] args) {
+        Graph2 graph2 = new Graph2();
+
+        graph2.addVertex("first");
+        graph2.addVertex("second");
+
+        graph2.containsVertex("first");
+
     }
 }
