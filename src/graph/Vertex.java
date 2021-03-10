eb8bd4bd7af9d233517d3084c792c90e9e116812
imadcollin <imad.collin@gmail.com>
2021-03-10 22:46:09 +0100

    Override hascode and equal method

--- src/graph/Vertex.java
+++ src/graph/Vertex.java
@@ -6,4 +6,33 @@ public class Vertex {
     public Vertex(String labal) {
         this.label = labal;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Graph2.class.hashCode();
+        result = prime * result + ((label == null) ? 0 : label.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Vertex other = (Vertex) obj;
+        if (!Graph2.class.equals(other.getClass()))
+            return false;
+        if (label == null) {
+            if (other.label != null)
+                return false;
+        } else if (!label.equals(other.label))
+            return false;
+        return true;
+    }
+
 }
