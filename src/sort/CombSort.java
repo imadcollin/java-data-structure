c9d2d7d4f9b23f8137c459f8817590aa30be3f65
imadcollin <imad.collin@gmail.com>
2021-03-17 23:40:21 +0100

    Refactoring

--- src/sort/CombSort.java
+++ src/sort/CombSort.java
@@ -7,18 +7,16 @@ public class CombSort {
 
     // GeegsOfGeeks
     public static void combSort(int arr[]) {
-        int gab = arr.length;
+        int gap = arr.length;
         int n = arr.length;
         boolean swap = true;
-        while (gab != 1 || swap == true) {
-            gab = getGap(gab);
+        while (gap != 1 || swap == true) {
+            gap = getGap(gap);
             swap = false;
 
-            for (int i = 0; i < n - gab; i++) {
-                if (arr[i] > arr[gab + i]) {
-                    int temp = arr[i];
-                    arr[i] = arr[gab + i];
-                    arr[gab + i] = temp;
+            for (int i = 0; i < n - gap; i++) {
+                if (arr[i] > arr[gap + i]) {
+                    swap(arr, i, gap + i);
                     swap = true;
                 }
             }
@@ -33,5 +31,12 @@ public class CombSort {
         if (gab < 1)
             return 1;
         return gap;
+    }
+
+    public static void swap(int arr[], int i, int y) {
+        int temp = arr[i];
+        arr[i] = arr[y];
+        arr[y] = temp;
+
     }
 }
