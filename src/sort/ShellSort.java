fe45e9a73ee97b81ec71edac4f053af2d24c7123
imadcollin <imad.collin@gmail.com>
2021-03-22 23:09:48 +0100

    Add shell sort method

--- src/sort/ShellSort.java
+++ src/sort/ShellSort.java
@@ -7,6 +7,18 @@ public class ShellSort {
     }
 
     public static int[] shellSort(int arr[]) {
+        int n = arr.length;
+        for (int interval = n / 2; interval > 0; interval /= 2) {
+            for (int i = interval; i < n; i++) {
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= interval && arr[j - interval] > temp; j -= interval) {
+
+                    arr[j] = arr[j - interval];
+                }
+                arr[j] = temp;
+            }
+        }
         return arr;
     }
 
