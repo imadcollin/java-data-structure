771ca9509c1bc1f84ce7574050488d936b854636
imadcollin <imad.collin@gmail.com>
2021-03-23 21:39:53 +0100

    Add initial linear sort struc

---
+++ src/sort/LinearSearch.java
@@ -0,0 +1,17 @@
+package sort; 
+class LinearSearch {
+    public static void main(String[] args) {
+        print(linearSearch(new int[] { 1, 3, 4, 2, 6, 5 }));
+
+    }
+
+    public static int[] linearSearch(int[] arr) {
+        return arr;
+    }
+
+    public static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.println(i + " ");
+        }
+    }
+}
