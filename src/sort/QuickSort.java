4af0205902e76b8f8f8784333e3b934f7770d9ec
imadcollin <imad.collin@gmail.com>
2021-03-24 21:34:46 +0100

    Add quicksort structure

---
+++ src/sort/QuickSort.java
@@ -0,0 +1,17 @@
+package sort;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        quickSort(new int[] { 1, 2, 4, 3, 6, 5 });
+    }
+
+    public static int[] quickSort(int arr[]) {
+        return arr;
+    }
+
+    public static void print(int arr[]) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
