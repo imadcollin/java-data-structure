711f3f06ad418dac7d24cbf17cc208fef1e24f30
imadcollin <imad.collin@gmail.com>
2021-03-26 20:38:15 +0100

    Add initial file structural for merge sort

---
+++ src/sort/MergeSort.java
@@ -0,0 +1,23 @@
+package sort;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 4, 3, 7, 5 };
+        mergeSort(arr);
+        print(arr);
+    }
+
+    public static int[] merge(int arr[]) {
+        return arr;
+    }
+
+    public static int[] mergeSort(int arr[]) {
+        return arr;
+    }
+
+    public static void print(int arr[]) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
