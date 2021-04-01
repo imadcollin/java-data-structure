b389af0e16f587db7350a26a2600619bddace566
imadcollin <imad.collin@gmail.com>
2021-04-01 23:41:45 +0200

    Add dequeue functions

--- src/DS1/queue/CircularQueue.java
+++ src/DS1/queue/CircularQueue.java
@@ -1,5 +1,7 @@
 package DS1.queue;
 
+import java.util.Arrays;
+
 public class CircularQueue {
     private int arr[];
     private int front, rear, capacitiy;
@@ -40,6 +42,26 @@ public class CircularQueue {
         }
     }
 
+    public void dequeue() {
+        int el = -1;
+        if (isEmpty()) {
+            System.out.println("The queue is Empty the operation is not possible...!");
+            System.exit(-1);
+
+        } else if (front == capacitiy - 1) {
+            el = arr[front];
+            front = 0;
+        } else if (front == rear) {
+            el = arr[front];
+            front = rear = -1;
+        } else {
+
+            el = arr[front];
+            arr = Arrays.copyOfRange(arr, 1, capacitiy + 1);
+        }
+        System.out.println("Element: " + el + " is deleted...!");
+    }
+
     public void print() {
         if (isEmpty()) {
             System.out.println("The queue is Empty nothing to print...!");
@@ -62,11 +84,18 @@ public class CircularQueue {
         circularQueue.enqueu(5);
         circularQueue.print();
 
+        circularQueue.dequeue();
+        circularQueue.print();
+
         circularQueue.enqueu(1);
         circularQueue.enqueu(2);
         circularQueue.enqueu(3);
         circularQueue.enqueu(4);
         circularQueue.enqueu(5);
+
+        circularQueue.dequeue();
+        circularQueue.dequeue();
+        circularQueue.dequeue();
         circularQueue.print();
 
     }
