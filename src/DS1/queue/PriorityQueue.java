000df957bb5ba0adb292f4f1166768b63b7aed0c
imadcollin <imad.collin@gmail.com>
2021-04-02 18:18:11 +0200

    Add priorityqueue skeleton

---
+++ src/DS1/queue/PriorityQueue.java
@@ -0,0 +1,33 @@
+package DS1.queue;
+
+import java.util.ArrayList;
+
+public class PriorityQueue {
+    private ArrayList arrayList;
+
+    public PriorityQueue(int size) {
+        arrayList = new ArrayList<>();
+    }
+
+    public void heapofiy() {
+
+    }
+
+    public void insertNode(int data) {
+
+    }
+
+    public void deleteNode(int data) {
+
+    }
+
+    public void print() {
+        for (Object object : arrayList) {
+            System.out.print(object + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
