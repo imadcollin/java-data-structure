c62f1eb6c2cbc8376a9adf60ecbd37b6e66bd4b4
imadcollin <imad.collin@gmail.com>
2021-04-05 21:13:00 +0200

    Add insert method

--- src/DS1/queue/PriorityQueue.java
+++ src/DS1/queue/PriorityQueue.java
@@ -3,18 +3,39 @@ package DS1.queue;
 import java.util.ArrayList;
 
 public class PriorityQueue {
-    private ArrayList arrayList;
+    private ArrayList<Integer> arrayList;
 
     public PriorityQueue(int size) {
         arrayList = new ArrayList<>();
     }
 
-    public void heapofiy() {
+    public void heapify(ArrayList<Integer> arr, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
 
+        if (left < arr.size() && arr.get(left) > arr.get(largest))
+            largest = left;
+        if (right < arr.size() && arr.get(right) > arr.get(largest))
+            largest = right;
+
+        if (largest != i) {
+            int temp = arr.get(largest);
+            arr.set(largest, arr.get(i));
+            arr.set(i, temp);
+            heapify(arr, largest);
+        }
     }
 
     public void insertNode(int data) {
-
+        if (arrayList.size() == 0) {
+            arrayList.add(data);
+        } else {
+            arrayList.add(data);
+            for (int i = arrayList.size() / 2 - 1; i >= 0; i--) {
+                heapify(arrayList, i);
+            }
+        }
     }
 
     public void deleteNode(int data) {
@@ -22,12 +43,19 @@ public class PriorityQueue {
     }
 
     public void print() {
-        for (Object object : arrayList) {
-            System.out.print(object + " ");
+        for (int el : arrayList) {
+            System.out.print(el + " ");
         }
     }
 
     public static void main(String[] args) {
+        PriorityQueue priorityQueue = new PriorityQueue(5);
+        priorityQueue.insertNode(3);
+        priorityQueue.insertNode(4);
+        priorityQueue.insertNode(9);
+        priorityQueue.insertNode(5);
+        priorityQueue.insertNode(2);
 
+        priorityQueue.print();
     }
 }
