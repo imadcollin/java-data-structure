65d8bf2b870fe25753b881c3b2794405ac345296
imadcollin <imad.collin@gmail.com>
2021-04-07 23:40:07 +0200

    Add insert front and rear

--- src/DS1/queue/Deque.java
+++ src/DS1/queue/Deque.java
@@ -13,6 +13,46 @@ public class Deque {
         this.size = size;
     }
 
+    boolean isEmpty() {
+        return front == -1;
+    }
+
+    boolean isFull() {
+        return (front == 0 && rear == size - 1);
+    }
+
+    void insertFront(int data) {
+        if (isFull()) {
+            System.out.println("The Deque is full");
+            return;
+        }
+        if (front == -1) {
+            front = 0;
+            rear = 0;
+        } else if (front == 0) {
+            front = size - 1;
+        } else {
+            front = front - 1;
+        }
+        arr[front] = data;
+    }
+
+    void insertRear(int data) {
+        if (isFull()) {
+            System.out.println("The Deque is full");
+            return;
+        }
+        if (front == -1) {
+            front = 0;
+            rear = 0;
+        } else if (rear == 0) {
+            rear = size - 1;
+        } else {
+            rear = rear + 1;
+        }
+        arr[rear] = data;
+    }
+
     void print(int arr[]) {
         Arrays.asList(arr).stream().forEach(x -> System.out.print(x + " "));
     }
