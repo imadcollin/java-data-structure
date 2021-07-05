ac4fc87afcc8c51be22bf76b0e11d3c078a2a952
imadcollin <imad.collin@gmail.com>
2021-07-05 23:07:07 +0200

    Rename class name

--- src/java11/strings/strings.java
+++ src/java11/strings/strings.java
@@ -1,7 +1,7 @@
 package java11.strings;
 import java.util.List;
 import java.util.stream.Collectors;
-public class strings {
+public class Strings {
     public static void main(String[] args) {
         String str2 = " str1 \n \n str2 \n str3.";
         List<String> lines = str2.lines().filter(line -> !line.isBlank()).map(String::strip)
