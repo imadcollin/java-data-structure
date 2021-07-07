4bf6f7c6d790143b05f509cf71f3ba61b450c0e1
imadcollin <imad.collin@gmail.com>
2021-07-07 16:28:13 +0200

    Feature test method to own class

---
+++ src/test/java/test/java/java11/FeatureTest.java
@@ -0,0 +1,16 @@
+package test.java.java11;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FeatureTest {
+    @Test
+    public void passed_test(){
+
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void failed_test(){
+        Assert.assertFalse(true);
+    }
+}
