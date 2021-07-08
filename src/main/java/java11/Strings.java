1984ca5ec1022eca5d930808bd4856c54a128fc3
imadcollin <imad.collin@gmail.com>
2021-07-08 20:53:33 +0200

    Replace output with Junit

--- src/main/java/java11/Strings.java
+++ src/main/java/java11/Strings.java
@@ -1,12 +1,21 @@
 package java11;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.util.List;
 import java.util.stream.Collectors;
+
 public class Strings {
-    public static void main(String[] args) {
+    @Test
+    public void strings() {
         String str2 = " str1 \n \n str2 \n str3.";
         List<String> lines = str2.lines().filter(line -> !line.isBlank()).map(String::strip)
                 .collect(Collectors.toList());
         assert (lines).contains("str1");
-        System.out.println(lines);
+        Assert.assertFalse(lines.contains("\n"));
+        Assert.assertFalse(lines.contains(" "));
+        Assert.assertEquals(3, lines.size());
+        Assert.assertNotSame(lines.get(0)+lines.get(1)+lines.get(2), str2);
     }
 }
