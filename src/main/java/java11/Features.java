94d28d01a044fdc29d20b534f807f97bb86ec926
imadcollin <imad.collin@gmail.com>
2021-07-08 21:36:21 +0200

    Move single methods to feature class

--- src/main/java/java11/Features.java
+++ src/main/java/java11/Features.java
@@ -1,28 +1,32 @@
 package java11;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Features {
-    public static void main(String[] args) {
-        predicate();
-        localVariable();
+
+    public List<String> stringsWithoutBlanks(String str) {
+       return  str.lines().filter(line -> !line.isBlank()).map(String::strip)
+                .collect(Collectors.toList());
+    }
+    public List<String> predicate(List<String> list) {
+
+      return list.stream().filter(Predicate.not(String::isBlank)).collect(Collectors.toList());
     }
 
-    public static void predicate() {
-        List<String> list = Arrays.asList(" ", " \n", "str1\n str2\n str3");
-        list.stream().forEach(System.out::print);
-
-        List listWithout = list.stream().filter(Predicate.not(String::isBlank)).collect(Collectors.toList());
-        listWithout.stream().forEach(System.out::print);
-    }
-
-    public static void localVariable() {
+    public void localVariable() {
         List<String> strList = Arrays.asList("java", "python");
         List<String> strUppderCase = strList.stream().map(x -> x.toUpperCase()).collect(Collectors.toList());
         strUppderCase.stream().forEach(System.out::print);
     }
-}
+
+        public void CollectiontoArray(){
+                List<String> list = Arrays.asList("first", "second");
+                String[] array = list.toArray(String[]::new);
+                for (String string : array) {
+                    System.out.println(string);
+                }
+        }
+    }
--- src/test/java/test/java/java11/FeatureTest.java
+++ src/test/java/test/java/java11/FeatureTest.java
@@ -1,16 +1,52 @@
 package test.java.java11;
 
+import java11.Features;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class FeatureTest {
+    private Features features;
+
+    @Before
+    public void init() throws Exception {
+         features = new Features();
+    }
+
     @Test
-    public void passed_test(){
+    public void passed_test() {
 
         Assert.assertTrue(true);
     }
+
     @Test
-    public void failed_test(){
+    public void failed_test() {
         Assert.fail();
     }
+
+    @Test
+    public void strings() {
+        String str = " str1 \n \n str2 \n str3.";
+        List<String> lines = features.stringsWithoutBlanks(str);
+
+        assert (lines).contains("str1");
+        Assert.assertFalse(lines.contains("\n"));
+        Assert.assertFalse(lines.contains(" "));
+        Assert.assertEquals(3, lines.size());
+        Assert.assertNotSame(lines.get(0) + lines.get(1) + lines.get(2), str);
+    }
+
+    @Test
+    public void predicate() {
+        List<String> list = Arrays.asList(" ", " \n", "str1\n str2\n str3");
+        List<String> listOFStringWithoutBlanks = features.predicate(list);
+        Assert.assertNotSame(list, listOFStringWithoutBlanks);
+        Assert.assertFalse(listOFStringWithoutBlanks.contains("\n"));
+        Assert.assertFalse(listOFStringWithoutBlanks.contains(null));
+    }
+
 }
